8afd340b7d3fab60529a2890fce176316fe7a5db
Lucia Rigoni Freire <luciarigoni@live.com>
2023-02-08 15:46:48 -0300

    Form using Swift UI

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scheduled: Bool = false
+    @State private var manuallyEnabledTillTomorrow: Bool = false
+    @State private var colorTemperature: CGFloat = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                
+                Section(header: Text("Night shift automatically shift colors Night shift automatically shift colors Night shift automatically shift colors Night shift automatically shift colors ").padding(5).lineLimit(nil)) {
+                    Toggle(isOn: $scheduled) {
+                        Text("Scheduled")
+                    }
+                    
+                    HStack {
+                        VStack {
+                            Text("From")
+                            Text("To")
+                        }
+                        
+                        Spacer()
+                        
+                        NavigationLink(destination:Text("Scheduled destination")) {
+                        VStack {
+                            Text("Sunset").foregroundColor(.blue)
+                            Text("Sunrise").foregroundColor(.blue)
+                            }
+                        }.fixedSize()
+                    }
+                }
+                
+                Section(header: Text("")) {
+                    Toggle(isOn: $manuallyEnabledTillTomorrow) {
+                        Text("Manually Enabled Till Tomorrow")
+                    }
+                }
+                
+                Section(header: Text("Temperature")) {
+                    HStack {
+                        Text("Less warm")
+                        Slider(value: $colorTemperature)
+                        Text("More warm")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
