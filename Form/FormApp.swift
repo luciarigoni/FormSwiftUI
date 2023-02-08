4f98ad8dc54a500696f5e74997ea15c4361bceae
Lucia Rigoni Freire <luciarigoni@live.com>
2023-02-08 15:27:54 -0300

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Lucia Rigoni Freire on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
